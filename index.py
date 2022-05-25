ffc0654d7a2620f4509f277825357bf7efebf447
15619 <1561900932@qq.com>
2022-05-26 01:33:26 +0800

    阿里云函数适配

--- index.py
+++
@@ -1,9 +0,0 @@
-try:
-    import aiohttp
-except ImportError:
-    import os
-    import sys
-    os.chdir(os.path.dirname(os.path.abspath(__file__)).split(__file__)[0])
-    os.system('pip install -r requirements.txt -t .')
-
-from main import run as handler
