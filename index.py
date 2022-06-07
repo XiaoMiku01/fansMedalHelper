d429cda8092d5f4ee9424e1f03fb54a90d9a742f
一心向晚 <54094119+XiaoMiku01@users.noreply.github.com>
2022-06-07 10:15:08 +0800

    fix #44 更改了 docker 运行时的执行顺序

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ import yaml
 
 if __name__ == '__main__':
     users = os.environ.get('USERS')
+    os.system("git pull")
     if users:
         with open("users.yaml", "w") as fw:
             yaml.dump(json.loads(users), fw)
         with open("users.yaml", "r") as fr:
             users = yaml.load(fr, Loader=yaml.FullLoader)
-    os.system("git pull")
     os.system("python main.py")
