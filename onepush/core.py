585dfd91ec7ea456581e9c90294a512d20a1fe64
15619 <1561900932@qq.com>
2022-07-29 23:54:02 +0800

    add support socks4/5 porxy

--- onepush/core.py
+++ onepush/core.py
@@ -9,6 +9,8 @@ from loguru import logger
 
 # import requests
 from aiohttp import ClientSSLError, ClientSession, TCPConnector
+from aiohttp_socks import ProxyConnector
+
 # from requests.exceptions import SSLError
 
 from .exceptions import NoSuchNotifierError
@@ -67,18 +69,26 @@ class Provider(object):
     # @staticmethod
     async def request(self, method, url: str, **kwargs):
         # session = requests.Session()
-
-        session = ClientSession() if not self.proxy else ClientSession(connector=TCPConnector(verify_ssl=False))
-        response = None
+        # session = (
+        #     ClientSession() if not self.proxy else ClientSession(connector=TCPConnector(verify_ssl=False))
+        # )
+        # response = None
         try:
             if self.proxy:
-                response = await session.request(method, url.replace('https', 'http'), proxy=self.proxy, **kwargs)
+                connector = ProxyConnector.from_url(self.proxy)
+                session = ClientSession(connector=connector)
+                response = await session.request(method, url, **kwargs)
             else:
+                session = ClientSession()
                 response = await session.request(method, url, **kwargs)
             # log.debug('Response: {}'.format(response.text))
         except ClientSSLError as e:
             log.error(e)
-            session = ClientSession(connector=TCPConnector(verify_ssl=False))
+            if self.proxy:
+                connector = ProxyConnector.from_url(self.proxy, verify_ssl=False)
+            else:
+                connector = TCPConnector(verify_ssl=False)
+            session = ClientSession(connector=connector)
             response = await session.request(method, url.replace('https', 'http'), proxy=self.proxy, **kwargs)
             # log.debug('Response: {}'.format(response.text))
         except Exception as e:
