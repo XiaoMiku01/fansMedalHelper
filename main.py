07958d236c07e1881a8cfc44a92d7cb907476d38
Jkkoi <jkkoi@jkkoi.top>
2023-02-05 11:44:26 +0800

    func: No need to re-edit files when upgrading

--- main.py
+++ main.py
@@ -20,9 +20,21 @@ try:
         users = json.loads(os.environ.get("USERS"))
     else:
         import yaml
+        import shutil
+
+        if not os.path.exists('users.yaml'):
+            shutil.copy('users.yaml.default', 'users.yaml')
+            assert False, "未找到配置文件，配置文件已重新生成，请编辑 users.yaml"
 
         with open('users.yaml', 'r', encoding='utf-8') as f:
             users = yaml.load(f, Loader=yaml.FullLoader)
+        with open('users.yaml.default', 'r', encoding='utf-8') as fd:
+            users_default = yaml.load(fd, Loader=yaml.FullLoader)
+            if not users.get('CONFIG', {}).get('VERSION', 0) >= users_default.get('CONFIG', {}).get('VERSION', 0):
+                shutil.copy('users.yaml', 'users.yaml.old')
+                shutil.copy('users.yaml.default', 'users.yaml')
+                assert False, "配置文件已更新，旧文件已保存到 users.yaml.old"
+
     assert users['ASYNC'] in [0, 1], "ASYNC参数错误"
     assert users['LIKE_CD'] >= 0, "LIKE_CD参数错误"
     # assert users['SHARE_CD'] >= 0, "SHARE_CD参数错误"
